ef758ecdae559898e79d5f3e109af2b98897285a
CRXAI <33507428+CRXAI@users.noreply.github.com>
2018-01-02 14:25:52 +0800

    Update build_etongdai_andr.sh

--- Scripts/Andr_Scripts/build_etongdai_andr.sh
+++ Scripts/Andr_Scripts/build_etongdai_andr.sh
@@ -378,6 +378,4 @@ CLEAN
 RESET
 setconfig
 package
-# package "com.stateunion.p2p.etongdai" "易通贷理财" "3.0.5" "3006" "yingxiao1001" "MTA3MTQwMjA=" "production" "Release" "liushuo" "true" | tee andr.log
-# package "com.stateunion.p2p.etongdai" "易通贷理财" "3.0.5" "3006" "test" "" "test" "Debug" "liushuo" "false"
 
